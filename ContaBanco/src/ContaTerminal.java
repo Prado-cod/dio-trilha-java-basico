391061563581d3c14c68e88593509f1f898d6d82
PradoDev <caiquepradodossantos123@gmail.com>
2025-07-04 17:47:16 -0300

    Código finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new  Scanner (System.in);
+        System.out.println("Bem-vindo, ao banco do brasileiro!!");
+
+        System.out.println("Por favor, nós informe o seu nome.");
+        String name = scanner.nextLine();
+
+        System.out.println("Por favor, informe a sua agência.");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, informe o número da sua conta.");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, informe o valor que deseja sacar.");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência: " + agencia + ", conta: " + conta + " e seu saldo " + saldo + " já esta disponivel para saque. ");
     }
 }
